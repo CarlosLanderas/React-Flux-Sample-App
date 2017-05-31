169d9aa9c20fe4f831939f33b0edd418df38cf53
unknown <carlos.landeras@gmail.com>
2017-05-31 21:47:18 +0200

    Initial upload

---
+++ src/actions/authorActions.js
@@ -0,0 +1,33 @@
+"use strict";
+
+var Dispatcher = require('../dispatcher/appDispatcher');
+var AuthorsApi = require('../api/authorApi');
+var ActionTypes = require('../constants/actionTypes');
+
+var AuthorActions = {
+    createAuthor: function (author) {
+        var newAuthor = AuthorsApi.saveAuthor(author);
+        Dispatcher.dispatch({
+            actionType: ActionTypes.CREATE_AUTHOR,
+            author: newAuthor
+        });
+    },
+
+    updateAuthor: function (author) {
+        var updatedAuthor = AuthorsApi.saveAuthor(author);
+        Dispatcher.dispatch({
+            actionType: ActionTypes.UPDATE_AUTHOR,
+            author: updatedAuthor
+        });
+    },
+
+    deleteAuthor: function (id) {
+       AuthorsApi.deleteAuthor(id);
+       Dispatcher.dispatch({
+            actionType: ActionTypes.DELETE_AUTHOR,
+            id: id
+        });
+    }
+};
+
+module.exports = AuthorActions;
---
+++ src/api/authorData.js
@@ -0,0 +1,20 @@
+    module.exports = {
+	authors: 
+	[
+		{
+			id: 'william-shakespeare', 
+			firstName: 'William', 
+			lastName: 'Shakespeare'
+		},	
+		{
+			id: 'franz-kafka', 
+			firstName: 'Franz', 
+			lastName: 'Kafka'
+		},	
+		{
+			id: 'oscar-wilde', 
+			firstName: 'oscar', 
+			lastName: 'wilde'
+		}
+	]
+};
